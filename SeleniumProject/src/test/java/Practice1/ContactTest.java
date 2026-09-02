1f32209c2ebf66f4d32260b13c116dbef90bf9c5
ksaddamr <149499725+ksaddamr@users.noreply.github.com>
2026-09-02 16:54:48 +0530

    it is 1st commit

---
+++ SeleniumProject/src/test/java/Practice1/ContactTest.java
@@ -0,0 +1,17 @@
+package Practice1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	
+	public void createContactTest() {
+		System.out.println("execute createContactTest");
+	}
+
+	@Test
+	public void modifyContactTest() {
+		System.out.println("execute modifyContactTest");
+	}
+}
